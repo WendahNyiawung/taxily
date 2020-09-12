b11f71e4cbea0a2316fa0614ad92ab23e21167c9
WendahNyiawung <wendahdesmond@gmail.com>
2020-09-12 14:31:30 +0100

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,61 @@
+const sr = ScrollReveal();
+
+sr.reveal('.logo',{
+    
+    interval:200,
+    origin:'bottom',
+    distance:'20px',
+    duration:2000
+});
+
+sr.reveal('.nav-links',{
+    
+    interval:200,
+    origin:'top',
+    distance:'20px',
+    duration:2000,
+    //delay:800
+});
+
+sr.reveal('.phone',{
+    
+    interval:200,
+    origin:'right',
+    distance:'20px',
+    duration:2000
+});
+
+sr.reveal('.book-btn',{
+    
+    interval:200,
+    origin:'bottom',
+    //distance:'20px',
+    duration:2000
+});
+
+sr.reveal('.carousel-caption',{
+    
+    interval:200,
+    origin:'bottom',
+    distance:'20px',
+    duration:2000,
+    delay:800
+});
+
+sr.reveal('.booking-form',{
+    
+    interval:200,
+    origin:'bottom',
+    distance:'20px',
+    duration:2000,
+    delay:1000
+});
+
+sr.reveal('.card',{
+    
+    interval:200,
+    origin:'bottom',
+    distance:'20px',
+    duration:1000,
+    delay:1000
+});
